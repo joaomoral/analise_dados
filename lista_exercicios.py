215f455d8b5fd317d13a8358c6e044c589b57447
joaomoral <moraljoao00@gmail.com>
2026-02-19 09:15:11 -0300

    exercício lista e dicionário

---
+++ lista_exercicios.py
@@ -0,0 +1,16 @@
+#21
+nomes = ["Ana", "Pedro", "Maria","João"]
+
+#22
+for nome in nomes:
+    print(nome)
+
+#23 
+nomes_maiusculo = ["ANA", "PEDRO", "MARIA","JOÃO"]
+
+
+#24
+numeros = range(1,20)
+for n in numeros:
+    if n % 2 == 0:
+        print(n)
